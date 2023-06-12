335c300a46c470b145509d04f3da8c0020cbf6ee
Valerie PerezPimentel <vperezpimentel@xpanxion.com>
2023-06-12 11:20:09 -0400

    quiz

--- dbConnection.js
+++ dbConnection.js
@@ -1,3 +1,11 @@
 const mysql = require('mysql2');
 
 // create pool or connection
+exports.pool = mysql.createPool(
+    {
+        host: '127.0.0.1',
+        port: '3306',
+        user: 'root',
+        password:'Ositodepeluche1224!',
+        database: 'sql-exam',
+    }).promise()
--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const pool = require('./dbConnection');
 const path = require('path');
 const PORT = 8080;
 
@@ -17,32 +18,57 @@ const options = {
 
 // #3 helper function 'getColor`
 const getColor = (fruit) => {
+
+  if (Object.keys(options) === fruit){
+    return Object.values(options); 
+  }
+  if (!Object.keys(options)){
+    console.log('Not found')
+  }
+
 }
+
 
 // #1 serve the colors.html page when /colors is visited
 // DO NOT USE express.static
-app.get('/colors', () => {
+app.get('/colors', (req, res) => {
+const ABSOLUTE_PATH = path.join(__dirname, './client/colors.html')
+res.status(200).send(ABSOLUTE_PATH);
   
 });
 
 // #2 & #4 handle POST requests to /colors
-app.post('/colors', () => {
-  
+app.post('/colors', (req, res) => {
+   let fruit = req.body; 
+   let color = getColor(fruit);
+
+   res.send(color)
+
 });
 
 // #6 serve styles.css - DO NOT use express.static()
 app.get('/styles.css', () => {
+  const ABSOLUTE_PATH = path.join(__dirname, './client/styles.css')
+  res.status(200).send(ABSOLUTE_PATH);
 
 });
 
 // #5 Update functionality to database
-app.put('/colors/:id/:fruit', () => {
+app.put('/colors/:id/:fruit', async (req, res) => {
+  let { car_id, fruit } = req.params;
+  let color = getColor(fruit);
+
+  const [carColor] = await pool.execute('UPDATE cars SET color = ? WHEN car_id =?', [color, car_id])
+
+  res.status(200).send({message: 'Color updated'})
 
 });
 
 // #7 unknown routes - 404 handler
 // research what route to serve this for
-app.get('', () => {
+app.get('**', (req, res) => {
+  const ABSOLUTE_PATH = path.join(__dirname, './client/404.html')
+  res.status(200).send(ABSOLUTE_PATH);
   
 })
 
